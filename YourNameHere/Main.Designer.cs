db57c8fdd74ccd350f8ae3695506b34a3def2ab0
bonny1992 <lh_firepower@hotmail.com>
2018-02-13 11:29:36 +0100

    Seconda commit di prova

--- YourNameHere/Main.Designer.cs
+++ YourNameHere/Main.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 448);
+            this.ClientSize = new System.Drawing.Size(1046, 504);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "YourNameHere";
             this.Text = "YourNameHere";
